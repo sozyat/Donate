cfcfdbdffd418cbd8e4d9b87c21581abf0f4fb27
sozyat <sozyat@gmail.com>
2018-04-15 17:10:52 -0500

    initial commit

--- app/src/main/java/com/example/ranji/donate/MainActivity.java
+++ app/src/main/java/com/example/ranji/donate/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.ranji.donate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        TextView textView = (TextView) findViewById(R.id.register);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Register.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/ranji/donate/Register.java
@@ -0,0 +1,15 @@
+package com.example.ranji.donate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.additional_info);
+    }
+}
